15d5a3804605aa686f534041786f0b6e84188856
lalitmee <lalitkumar.meena.lk@gmail.com>
2018-04-09 23:45:40 +0530

    Some Modifications
    Added dummy `header`,`footer` and logo for showing
    the use of semantic-ui-react

--- src/Componenets/App/App.js
+++ src/Componenets/App/App.js
@@ -1,15 +1,37 @@
-import React, { Component } from 'react';
-import Footer from '../Footer/Footer.js';
-import Header from '../Header/Header.js';
-import './App.css';
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+
+import Footer from "../Footer/Footer.js";
+import Header from "../Header/Header.js";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-        <p>This is App </p>
+        <Container style={{ fontSize: "20px" }}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
+          <p>This is App </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
+          <p>This is App </p>
+        </Container>
         <Footer />
       </div>
     );
