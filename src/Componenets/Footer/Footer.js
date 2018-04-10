afbbb14eee0005aebcd60eda28ff3719a8467edb
Aditya Verma <adi004.zz@gmail.com>
2018-04-10 21:10:54 +0530

    Added Bootstrap dependencies and example, removed Semantic UI

--- src/Componenets/Footer/Footer.js
+++ src/Componenets/Footer/Footer.js
@@ -1,101 +1,99 @@
-import React from "react";
-import {
-	Container,
-	Grid,
-	List,
-	Image,
-	Segment,
-	Divider,
-	Icon
-} from "semantic-ui-react";
+import React, {Component} from "react";
 
-import "semantic-ui-css/semantic.min.css";
-import logo from "../../assets/images/logo.png";
-import AppStore from "../../assets/images/apple-store.png";
-import GooglePlay from "../../assets/images/google-play.png";
+import "bootstrap/dist/css/bootstrap.css";
+import "./Footer.css";
 
-const Footer = () => (
-	<Segment
-		vertical
-		style={{
-			padding: "2em 0em",
-			marginTop: "3em",
-			marginBottom: "0",
-			backgroundColor: "#282C35"
-		}}
-	>
-		<Container>
-			<Grid doubling stackable columns={2}>
-				<Grid.Row columns={2}>
-					<Grid.Column textAlign="left">
-						<Image
-							as="a"
-							href=""
-							src={logo}
-							style={{
-								width: "50px",
-								height: "auto",
-								marginTop: "-10px"
-							}}
-						/>
-					</Grid.Column>
-					<Grid.Column textAlign="right">
-						<List horizontal link inverted relaxed>
-							<List.Item as="a" target="_blank" href="">
-								<Image
-									centered
-									src={AppStore}
-									style={{ width: "120px", height: "auto" }}
-								/>
-							</List.Item>
+class Footer extends Component {
+	render(){
+		return (
+			// <!--Footer-->
+		<footer className="page-footer font-small stylish-color-dark pt-4 mt-4">
 
-							<List.Item as="a" target="_blank" href="">
-								<Image
-									centered
-									src={GooglePlay}
-									style={{ width: "120px", height: "auto" }}
-								/>
-							</List.Item>
-						</List>
-					</Grid.Column>
-				</Grid.Row>
-			</Grid>
-			<Divider />
-			<Grid doubling floated="left" centered inverted stackable>
-				<Grid.Row columns="equal">
-					<Grid.Column floated="left">
-						<List horizontal link inverted relaxed>
-							<List.Item as="a">Terms of Service </List.Item>
-							<List.Item as="a">Privacy Policy</List.Item>
+			{/* <!--Footer Links--> */}
+			<div className="container text-center text-md-left">
+				<div className="row">
 
-							<List.Item as="a">FAQ's</List.Item>
-						</List>
-					</Grid.Column>
-					<Grid.Column floated="right" textAlign="right">
-						<List horizontal link inverted>
-							<List.Item as="a" target="_blank">
-								<Icon inverted circular name="facebook" />
-							</List.Item>
+					{/* <!--First column--> */}
+					<div className="col-md-4">
+						<h5 className="text-uppercase mb-4 mt-3 font-weight-bold">Footer Content</h5>
+						<p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit
+							amet, consectetur adipisicing elit.</p>
+					</div>
+					{/* <!--/.First column--> */}
 
-							<List.Item as="a" target="_blank">
-								<Icon inverted circular name="instagram" />
-							</List.Item>
+					<hr className="clearfix w-100 d-md-none"/>
 
-							<List.Item as="a" target="_blank">
-								<Icon inverted circular name="twitter" />
-							</List.Item>
-						</List>
-					</Grid.Column>
-				</Grid.Row>
-			</Grid>
-			<Divider />
-			<Grid centered columns={2}>
-				<Grid.Column textAlign="center" style={{ color: "white" }}>
-					©2018. All Rights Reserved.
-				</Grid.Column>
-			</Grid>
-		</Container>
-	</Segment>
-);
+					{/* <!--Second column--> */}
+					<div className="col-md-2 mx-auto">
+						<h5 className="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+						<ul className="list-unstyled">
+							<li>
+								<a href="#!">Link 1</a>
+							</li>
+							<li>
+								<a href="#!">Link 2</a>
+							</li>
+							<li>
+								<a href="#!">Link 3</a>
+							</li>
+							<li>
+								<a href="#!">Link 4</a>
+							</li>
+						</ul>
+					</div>
+					{/* <!--/.Second column--> */}
+
+					<hr className="clearfix w-100 d-md-none"/>
+
+					{/* <!--Third column--> */}
+					<div className="col-md-2 mx-auto">
+						<h5 className="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+						<ul className="list-unstyled">
+							<li>
+								<a href="#!">Link 1</a>
+							</li>
+							<li>
+								<a href="#!">Link 2</a>
+							</li>
+							<li>
+								<a href="#!">Link 3</a>
+							</li>
+							<li>
+								<a href="#!">Link 4</a>
+							</li>
+						</ul>
+					</div>
+					{/* <!--/.Third column--> */}
+
+					<hr className="clearfix w-100 d-md-none"/>
+
+					{/* <!--Fourth column--> */}
+					<div className="col-md-2 mx-auto">
+						<h5 className="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+						<ul className="list-unstyled">
+							<li>
+								<a href="#!">Link 1</a>
+							</li>
+							<li>
+								<a href="#!">Link 2</a>
+							</li>
+							<li>
+								<a href="#!">Link 3</a>
+							</li>
+							<li>
+								<a href="#!">Link 4</a>
+							</li>
+						</ul>
+					</div>
+					{/* <!--/.Fourth column--> */}
+				</div>
+			</div>
+			{/* <!--/.Footer Links--> */}
+
+		</footer>
+            
+		)
+	}
+}
 
 export default Footer;
