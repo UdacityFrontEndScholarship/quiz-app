ed7bd46d998a472a338e5ffb902a8aa5aee6b019
Abhishek <abhishekakade@gmail.com>
2018-04-11 20:48:26 +0530

    added 3 bootstrap4 dependencies at the bottom of index.html for collapsible navbar button to work | made footer non sticky in Footer.css | added index.html link to the home button in Header.js

--- src/Componenets/Header/Header.js
+++ src/Componenets/Header/Header.js
@@ -15,7 +15,8 @@ class Header extends Component {
 					<div className="collapse navbar-collapse" id="navbarSupportedContent">
 						<ul className="navbar-nav mr-auto">
 						<li className="nav-item active">
-							<a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+							{/* link added for homepage */}
+							<a className="nav-link" href="../../public/index.html">Home <span className="sr-only">(current)</span></a>
 						</li>
 						<li className="nav-item">
 							<a className="nav-link" href="#">Link</a>
