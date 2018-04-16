6dd4896804fcd8d6af6e5c549dbe8a786b395d61
Abhishek <abhishekakade@gmail.com>
2018-04-16 19:10:19 +0530

    Added class  to Sign In button in Signin.js. Added class  to Sign Up button in Signup.js. Fixed Google/GitHub logo vertical alignment in buttons, changed it to middle. Fixed uneven spacing issues in Signin.js form. Added some Bootstrap 4 classNames in Form.js for better responsiveness.

--- src/Componenets/Form/Form.js
+++ src/Componenets/Form/Form.js
@@ -5,25 +5,25 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./Form.css";
 const Form = (props) => {
     return (
-        <div className="maincontainer" style={{ margin: "5em" }}>
+        <div className="maincontainer">
             <div className="container">
                 <div className="row justify-content-center">
-                    <div className="col-md-6 col-md-offset-4">
-                        <div className="card border border-dark">
-                            <div className="card-header border border-dark">
+                    <div className="col-xs-12 col-sm-12 col-md-9 col-lg-6 col-md-offset-4">
+                        <div className="card">
+                            <div className="card-header">
                                 <div className="container">
-                                    <div className="row border border-dark">
-                                        <div className="col-xs-6 col-md-6 col-lg-6 signup text-center" id="signup-link">
+                                    <div className="row">
+                                        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6 signup text-center" id="signup-link">
                                             <a onClick = {props.signUp}
                                                 href="#signup"
-                                                className="btn btn-lg btn-block btn-dark active"
+                                                className="btn btn-lg btn-block active font-weight-bold"
                                                 role="button" aria-pressed="true" id="signup">Sign Up</a>
 
                                         </div>
                                         <div
-                                            className="col-xs-6 col-md-6 col-lg-6 signin text-center" id="signin-link">
+                                            className="col-xs-6 col-sm-6 col-md-6 col-lg-6 signin text-center" id="signin-link">
                                             <a onClick = {props.logIn}
-                                                href="#signin" className="btn btn-lg btn-block btn-dark active" role="button" aria-pressed="true" id="signin">Sign In</a>
+                                                href="#signin" className="btn btn-lg btn-block active font-weight-bold" role="button" aria-pressed="true" id="signin">Sign In</a>
                                         </div>
                                     </div>
                                 </div>
