3da2b0e93baa7996c6685641b6c35e5e67357e5c
Abhishek <abhishekakade@gmail.com>
2018-04-16 08:53:08 +0530

    Fixed repeated code caused due to conflicts while merging

--- src/Componenets/Form/Form.js
+++ src/Componenets/Form/Form.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Signin from '../Signin/Signin.js';
 import Signup from '../Signup/Signup.js';
-
-<<<<<<< HEAD
 import "bootstrap/dist/css/bootstrap.css";
 import "./Form.css";
 const Form = (props) => {
@@ -39,51 +37,10 @@ const Form = (props) => {
                             {/* Form switch logic here */}
                             {props.selectForm?<Signup/> : <Signin/>} 
                         </div>
-=======
-import 'bootstrap/dist/css/bootstrap.css';
-import './Form.css';
-const Form = props => {
-  return (
-    <div className="maincontainer" style={{ margin: '80px' }}>
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-6 col-md-offset-4">
-            <div className="card">
-              <div className="card-header">
-                <div className="container">
-                  <div className="row">
-                    <div
-                      className="col-xs-6 col-md-6 col-lg-6 signup text-center"
-                      id="signup-link"
-                    >
-                      <a
-                        onClick={props.signUp}
-                        href="#signup"
-                        className="active"
-                        id="signup"
-                      >
-                        <strong>Sign Up</strong>
-                      </a>
->>>>>>> upstream/master
                     </div>
-                    <div
-                      className="col-xs-6 col-md-6 col-lg-6 signin text-center"
-                      id="signin-link"
-                    >
-                      <a onClick={props.logIn} href="#signin" id="signin">
-                        <strong>Sign In</strong>
-                      </a>
-                    </div>
-                  </div>
                 </div>
-              </div>
-              {/* Form switch logic here */}
-              {props.selectForm ? <Signup /> : <Signin />}
             </div>
-          </div>
         </div>
-      </div>
-    </div>
   );
 };
 
