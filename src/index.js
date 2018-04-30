3bfb97a151f8ab61d877f684ead553b9aba3dbff
Aditya Verma <adi004.zz@gmail.com>
2018-04-30 23:47:30 +0530

    Added Google oauth

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './Components/App/App.js';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import registerServiceWorker from './registerServiceWorker'; //Don't change this line
+// import registerServiceWorker from './registerServiceWorker'; //Don't change this line
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-registerServiceWorker(); //invoking ServiceWorker
+// registerServiceWorker(); //invoking ServiceWorker
