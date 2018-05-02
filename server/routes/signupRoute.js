ab8f2a34d3aa388da8eccfd319d5c5df4374aa44
kadumuri <vamshi2510.nitk@gmail.com>
2018-05-02 10:29:52 +0530

    added some server side code changes and made font changes to footer

---
+++ server/routes/signupRoute.js
@@ -0,0 +1,11 @@
+const userController = require('../controllers/userController');
+
+module.exports = app => {
+    app.post('/', function (req, res, next) {
+        /*
+      Receive the credentials here and have checks
+      */
+        res.send('Render home page/user dashboard after successful signup');
+    });
+}
+
